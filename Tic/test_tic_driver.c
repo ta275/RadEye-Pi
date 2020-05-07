b906efd5bd5a4640e71d5bd4220adaf46c76ec69
ta275 <38818430+ta275@users.noreply.github.com>
2020-05-07 18:52:18 -0400

    com push

--- Tic/test_tic_driver.c
+++ Tic/test_tic_driver.c
@@ -7,11 +7,11 @@ int main()
 
 	const char horizontal_sno [9] = "00286960";
 
-	uint32_t max_speed = 4000000;
+	uint32_t max_speed = 8000000;
 	uint32_t starting_speed = 0;
-	uint32_t max_decel = 40000;
-	uint32_t max_accel = 40000; 
-	uint8_t step_mode = 0;
+	uint32_t max_decel = 80000;
+	uint32_t max_accel = 80000; 
+	uint8_t step_mode = 1;
 	uint16_t current_limit = 1000;
 	int32_t curr_pos = 0;
 
