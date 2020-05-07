07efe7e75fb02058d461a0bb4d620df7a131f89b
ta275 <38818430+ta275@users.noreply.github.com>
2020-05-07 18:58:37 -0400

    com push

--- Tic/test_tic_driver.c
+++ Tic/test_tic_driver.c
@@ -7,13 +7,17 @@ int main()
 
 	const char horizontal_sno [9] = "00286960";
 
-	uint32_t max_speed = 8000000;
-	uint32_t starting_speed = 0;
-	uint32_t max_decel = 80000;
-	uint32_t max_accel = 80000; 
+
 	uint8_t step_mode = 1;
+
+	uint32_t max_speed = 2000000 * (step_mode+1);
+	uint32_t starting_speed = 0;
+	uint32_t max_decel = 40000 * (step_mode+1);
+	uint32_t max_accel = 40000 * (step_mode+1); 
 	uint16_t current_limit = 1000;
 	int32_t curr_pos = 0;
+
+	int32_t full_vertical = 83;
 
 	TicDriver* vertical = createTicDriver(vertical_sno, max_speed, starting_speed,
 	 max_decel, max_accel, step_mode, current_limit, curr_pos);
@@ -21,14 +25,12 @@ int main()
 	TicDriver* horizontal = createTicDriver(horizontal_sno, max_speed, starting_speed,
 	 max_decel, max_accel, step_mode, current_limit, curr_pos);
 
-	vertical->steps(vertical, 83);
-
-	sleep(2);
-
-	vertical->steps(vertical, -83);
-
-	sleep(2);
 	
-	vertical->deenergize(vertical);
+	vertical->steps(vertical, full_vertical*(step_mode+1));
+
+
+	vertical->steps(vertical, full_vertical*(step_mode+1));
+	
+	// vertical->deenergize(vertical);
 
 }
