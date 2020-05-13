af226f458cb17cca7e3d0223d14f617651637564
ta275 <38818430+ta275@users.noreply.github.com>
2020-05-13 14:55:09 -0400

    pc push

---
+++ Vision/Socket/echo_client.py
@@ -0,0 +1,8 @@
+import socket
+
+HOST = '10.148.3.11'  # The server's hostname or IP address
+PORT = 7777        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
