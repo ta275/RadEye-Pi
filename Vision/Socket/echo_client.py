de5ccaffec313e0b90dad505336e3f0e24fb0fa8
ta275 <38818430+ta275@users.noreply.github.com>
2020-05-13 15:03:42 -0400

    pc push

--- Vision/Socket/echo_client.py
+++ Vision/Socket/echo_client.py
@@ -6,3 +6,6 @@ PORT = 7777        # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
